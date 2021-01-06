2d83f12a79d86ea90201e7c831bd6109e4d45753
Werner Denzin <77076652+werner-denzin@users.noreply.github.com>
2021-01-06 19:28:44 -0300

    Added the build script for Python package

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="ai.tool", 
+    version="0.0.1",
+    author="Werner Denzin",
+    author_email="werner.cjd@gmail.com",
+    description="A Python package of auxiliary tools for Data Science projects",
+    url="https://github.com/werner-denzin/ai.tools"
+)

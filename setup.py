462eba7f2ab646632b2f35baa69c7df237948cc6
Werner Denzin <77076652+werner-denzin@users.noreply.github.com>
2021-01-07 09:26:51 -0300

    Added zip_safe attribute

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setuptools.setup(
     author="Werner Denzin",
     author_email="werner.cjd@gmail.com",
     description="A Python package of auxiliary tools for Data Science projects",
-    url="https://github.com/werner-denzin/ai.tools"
+    url="https://github.com/werner-denzin/ai.tools",
+    zip_safe=False
 )
